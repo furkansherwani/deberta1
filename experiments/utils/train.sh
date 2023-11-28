8a957408d79170de5a1e680e20904ac0ddd879ff
Furkan <furkan818@gmail.com>
2023-11-28 12:28:00 +0500

    ok

--- experiments/utils/train.sh
+++ experiments/utils/train.sh
@@ -105,7 +105,7 @@ cp -f $CONFIG $OUTPUT/model_config.json
 if [[ ! ${Predict,,} = 'true' ]]; then
   CMD=" --do_train --do_eval --do_predict"
 else
-  CMD=" "
+  CMD=" --do_predict"
 fi
 
 parameters="--task_name $Task $CMD \
